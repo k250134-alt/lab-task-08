eaf07e83639fdbf048e2497f574a08b2eec0713d
DANIYA ASIM <k250134@nu.edu.pk>
2025-10-23 09:27:30 +0500

    Add files via upload

---
+++ daniya lab task 8 q1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,space;
+	printf("****MULTIPLICATION TABLE****\n");
+	for(i=1;i<=5;i++)
+	{
+		printf("%d: ",i);
+		for(j=1;j<=5;j++)
+		{
+			printf("%4d",i*j);
+		}
+		printf("\n");
+	}
+	
+}
